f7e287956eb7eb20c11441a7c42b96758cf16da0
Rustam <ruslivaev05@gmail.com>
2022-09-25 02:21:40 +0600

    add deleteUser method

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -3,4 +3,14 @@ import db from "../db.js"
 export const getUsers = async (req, res) => {
     const users = await db.query(`SELECT * FROM  persons`)
     res.status(200).json({ users: users.rows })
-}
+};
+// export const addUsers = async (req, res) => {
+//     const {firstName,lastName,age, isFree, reatedAt, pdatedAt} = req.body;
+//     const users = await db.query(`INSERT INTO persons(firstName, lastName, age, isFree, reatedAt, pdatedAt ) values ($1, $2, $3, $4, $5, $6) RETURNING *`,[firstName, lastName, age, isFree, reatedAt, pdatedAt]);
+//     res.status(200).json({ users: users.rows })
+// }
+export const deleteUsers = async (req, res) => {
+    const id = req.body;
+    const users = await db.query(`DELETE FROM persons where id = $1`,[id])
+    res.status(200).json({ users: users.rows })
+};
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,13 @@
 import Router from "express";
 const router = new Router();
 import {getUsers} from "../controller/user-controller.js";
+import{deleteUsers} from "../controller/user-controller.js";
+// import {deleteUsers} from "../controller/user-controller.js";
 
 router.get('/users', getUsers)
-// router.get("/user/:id", userController.getUser)
+// router.post('/users', addUsers)
 // router.put("/user", userController.updateUser)
 // router.delete("/user/:id", userController.deleteUser)
+router.delete('/users/:id', deleteUsers)
 
 export default router;
