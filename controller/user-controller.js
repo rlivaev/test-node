c3a069beace10d4b804617bbebf85551dbbf1a97
Rustam <ruslivaev05@gmail.com>
2022-09-24 12:17:52 +0600

    add newperson

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,11 +1,10 @@
 import db from "../db"
 
 class userController{
-
     async createUser(req,res) {
-        const{firstName, lastName} = req.body;
-        console.log(firstName, lastName);
-        res.json("ok")
+        const {firstName, lastName} = req.body;
+        const newPerson = await db.query(`INSERT INTO persons (firstName lastName) value ($1, $2) RETURNING * `,[firstName, lastName]);
+        res.json(newPerson);
     }
     async getUser(req, res) {
 
