7c7ca67b834159b9bdcbb99a9aaa7a115f2fc7b2
Rustam <ruslivaev05@gmail.com>
2022-09-25 01:16:03 +0600

    db connection fixed

--- db.js
+++ db.js
@@ -1,21 +1,12 @@
-import * as pg from 'pg';
-const { Pool } = pg;
+import * as pg from 'pg'
+const { Pool } = pg.default
 
-const db = async () => {
-    console.log("pepsi");
-    try {
-        const pool = new Pool({
-            user: "postgres",
-            host: "localhost",
-            database: "testdb",
-            port: 5432
-        })
-        await client.connect()
-        console.log(res)
-        await client.end()
-    } catch (error) {
-        console.log(error)
-    }
-}
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'testdb',
+    port: 5432,
+    password:"admin"
+})
 
-export default db;
+export default pool;
