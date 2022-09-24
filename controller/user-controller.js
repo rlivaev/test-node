156b4c1bda72d6915ce5d00f9232d4c7bbff4c8d
Rustam <ruslivaev05@gmail.com>
2022-09-25 01:03:40 +0600

    dss

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,26 +1,7 @@
 import db from "../db.js"
 
-class userController{
-    async createUser(req,res) {
-        const {firstName, lastName} = req.body;
-        const newPerson = await db.query(`INSERT INTO persons (firstName lastName) value ($1, $2) RETURNING * `,[firstName, lastName]);
-        res.json(newPerson);
-    }
-    async getUsers(req, res) {
-        console.log("hi");
-        // const users = await db.query(`SELECT * FROM  persons`)
-        // res.json(users.rows);
-    };
-    async updateUser(req, res) {
-        const {id, firstName,lastName, isFree,createdAt,updatedAt} = req.body;
-        const user = await db.query(`UPDATE persons set firstName = $1, lastName = $2, id = $3 RETURNING *`,[firstName,lastName,id])
-        res.json(user.rows[0]);
-    }
-    async deleteUser(req, res) {
-        const id  =req.params.id
-        const user = await db.query(`DELETE * FROM persons where id = $1`,[id])
-        res.json(user.rows[0])
-    }
+export const getUsers = async (req, res) => {
+    const users = await db.query(`SELECT * FROM  persons`)
+    res.json(users.rows);
+    res.json({ users: users })
 }
-
-export default userController;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,8 @@
 import Router from "express";
 const router = new Router();
-import userController from "../controller/user-controller.js";
+import {getUsers} from "../controller/user-controller.js";
 
-router.get('/users', function (req, res) {
-   res.send(userController.getUsers);
-   console.log('coke');
-});
+router.get('/users', getUsers)
 // router.get("/user/:id", userController.getUser)
 // router.put("/user", userController.updateUser)
 // router.delete("/user/:id", userController.deleteUser)
