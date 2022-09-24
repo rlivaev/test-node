6b7667c7c4a81bf9eab338f9109d422ca5242177
Rustam <ruslivaev05@gmail.com>
2022-09-25 01:21:26 +0600

    chanched user-controller

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,6 +2,5 @@ import db from "../db.js"
 
 export const getUsers = async (req, res) => {
     const users = await db.query(`SELECT * FROM  persons`)
-    res.json(users.rows);
-    res.json({ users: users })
+    res.status(200).json({ users: users.rows })
 }
